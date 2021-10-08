1b2112692a63e0fa886822480f4228e835a485ac
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2021-10-08 18:20:27 -0500

    Correcion Ortogŕafica y Arreglo de error en Función Arterial

--- DONASANGRE.cpp
+++ DONASANGRE.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool EdadCheck, presionCheck, horaCheck, AyunoCheck; //Hola2
+bool edadCheck, presionCheck, horaCheck, ayunoCheck; // Booleanos como revisores de las funciones
 
 void edadPeso (){ //Proceso de entrada de datos de la edad y peso
     int edad, peso;
@@ -17,7 +17,8 @@ void edadPeso (){ //Proceso de entrada de datos de la edad y peso
             cin >> estatura;
             
             if (estatura >= 1.5){ //Se valida si cumple la estatura para continuar con el siguiente dato
-             cout << "Sus datos han sido guardados exitosamente" << endl;   
+             cout << "Sus datos han sido guardados exitosamente" << endl;
+             edadCheck = true; //Junto al mensaje de registro exitoso, validamo el revisor de esta función
             }
 
             /* ELSE: MENSAJES DE ERROR Y TERMINACION DE PROCESO */
@@ -36,16 +37,16 @@ void edadPeso (){ //Proceso de entrada de datos de la edad y peso
 
 void presionArterial (){ //Proceso validación de la presion arterial
     int sistolica, diastolica; 
-    bool ayuda;
+    bool ayuda; //Revisor de ayuda: Se activa cuando el usuario presiona 0 para solicitar ayuda
     do {
     cout << "Mida su presion arterial y llene le siguiente formulario \n Ingrese su presion Sistólica \n Presione 0 para ayuda:" << endl;
     cin >> sistolica;
-    ayuda = false;
+    ayuda = false; //Se reestablece el revisor
     cout << endl;
 
     if (sistolica == 0){
-        cout << "Su presion es medida con un número con el siguiente formato: 120 / 80 mm Hg \n donde: 120 es la presión sistólica y 80 la presión Diastólica" << endl;
-        ayuda = true;
+        ayuda = true; // Si seleccionó 0, se activa el revisor de ayuda y evita que pida el siguiente dato
+        cout << "Su presion es medida con un número con el siguiente formato: 120 / 80 mm Hg \n donde: 120 es la presión sistólica y 80 la presión Diastólica \n" << endl;
     }
 
     if (ayuda == false){
@@ -53,47 +54,52 @@ void presionArterial (){ //Proceso validación de la presion arterial
     cin >> diastolica;
     }
 
-    if (sistolica <= 120 && diastolica <= 80 && ayuda == false){
-        if (sistolica >= 90 && diastolica >= 60 && ayuda == false){
+    if (sistolica <= 120 && diastolica <= 80 && ayuda == false){ // Se valida que la presion no supere el límite normal (120/80)
+        if (sistolica >= 90 && diastolica >= 60 && ayuda == false){ // Se valida que no esté por debajo del mínimo normal (90/60)
             cout << "Su presión arterial es normal \n Sus datos han sido guardado con éxito" <<endl;
+            presionCheck = true;
         }
         else {
             cout << "Su presión arterial es demasiada baja, no puede completar el registro" << endl;
         }
     }
     else {
+        if (ayuda == false){ //Una comparativa simple para soluccionar el bug XD
         cout << "Su presión arterial es demasiada alta, no puede completar el resgistro" << endl;
+        }
     }
 
-    } while (sistolica == 0);
+    } while (sistolica == 0); // Si el primer dato es 0, significa que el usuario pidió ayuda, se muestra de nuevo las instrucciones
     
 }
 
 void horasDeSuenio(){
 	int horas;
-cout<< "indique cuanto durmio antes del examen"<<endl;
-do {
-cin>> horas;
-if (horas >= 5 or horas<= 24){
-	cout<<"tiempo de suenio aprobado"<<endl;
-}
-	else{
-	cout<<"el valor indicado no es valido"<<endl;
-	cout<<"ingrese otro valor nuevamente"<<endl;
-}
-}while(horas < 1 || horas > 24);
+    cout<< "Indique cuánto durmió antes del examen"<<endl;
+    do {
+        cin>> horas;
+        if (horas >= 5 or horas<= 24){
+            cout<<"Tiempo de sueño aprobado"<<endl; //En los mensajes cout sí permite la colocación de la letra ñ
+            horaCheck = true;
+        }
+        else{
+            cout<<"El valor indicado no es válido"<<endl;
+            cout<<"Ingrese otro valor nuevamente"<<endl;
+        }
+    }while(horas < 1 || horas > 24);
 }
 
 void tiempoDeAyuno(){
 	int ayuno;
-	cout<<"cuanto tiempo lleva en ayuno?"<<endl;
+	cout<<"¿Cuánto tiempo lleva en ayuno?"<<endl;
 	do{
 	cin>> ayuno;
 	if (ayuno> 7 && ayuno < 10 ){
-		cout<<"tiempo de ayuno adecuado"<<endl;
+		cout<<"Tiempo de ayuno adecuado"<<endl;
+        ayunoCheck = true;
 	}
 	else{
-		cout<<"el tiempo de ayuno no es indicado para el examen"<<endl;
+		cout<<"El tiempo de ayuno no es indicado para el examen, por favor ingrese una cantidad válida"<<endl; // Puse la instruccion de ingresar una cantidad nueva porque se veía rara
 	}
 	}while (ayuno<1 or ayuno> 10);
 	
